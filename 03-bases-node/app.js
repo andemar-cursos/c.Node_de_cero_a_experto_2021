4a251b3eb9f1491f9e1ea5f0d84bfeaa291fae83
Anderson <andemartinezdev@gmail.com>
2021-05-03 09:40:33 -0500

    04-04 -> Require
    - Se desarrollo un metodo, el cual necesitaba una libreria para su ejecucion.

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,9 +1,27 @@
+const fs = require('fs');
+
 
 console.clear();
 
-let numero = 5;
+let numero = 3;
 let iteracion = 10;
+let salida = "";
+
+console.log("===========")
+console.log(`Tabla del ${numero}`)
+console.log("===========")
+
 
 for(let i = 1; i <= iteracion; i++){
-    console.log(`${numero} x ${i} = ${numero*i}`);
+    salida += `${numero} x ${i} = ${numero*i}\n`;
 }
+
+console.log(salida);
+
+
+fs.writeFile(`tabla-${numero}.txt`, salida, null, (err)=> {
+
+    if(err) throw err;
+
+    console.log('tabla-5.txt creado');
+})
