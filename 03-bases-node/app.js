9033a72b57d4e3865ab002701559a285c08214bc
Anderson <andemartinezdev@gmail.com>
2021-05-03 16:58:33 -0500

    04-11 -> Colors Dependenci
    - Se uso la denpendencia colors que permite imprimir textos a color por consola.

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,11 +1,12 @@
 const { crearArchivo } = require('./helpers/multiplicar')
+const colors = require('colors');
 const argv = require('./config/yargs');
 
 
 console.clear();
 
 crearArchivo(argv.base, argv.listar)
-    .then((msg)  => console.log(msg + " Creado"))
+    .then((msg)  => console.log(msg.rainbow + " Creado"))
     .catch((msg) => console.log(msg + " - err"));
 
 
