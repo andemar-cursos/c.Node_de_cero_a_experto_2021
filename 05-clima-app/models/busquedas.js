e16e37358401846ce694d03aca24b50b91f77aa1
Anderson <andemartinezdev@gmail.com>
2021-05-05 15:14:46 -0500

    06-11 -> Llamado endpoint
    - Se hizo el llamado a un endpoint de mapbox.

--- 05-clima-app/models/busquedas.js
+++ 05-clima-app/models/busquedas.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const env = require('../environment/environment');
 
 class Busquedas {
 
@@ -8,11 +9,31 @@ class Busquedas {
         //TODO: Leer DB
     }
 
+    get paramsMapbox() {
+        return {
+            'access_token': env.MAPBOX,
+            'limit': 5,
+            'language': 'es',
+        };
+    }
+
     async ciudad(lugar = '') {
 
         // Peticion http
+        try {
+            const instance = axios.create({
+                baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${lugar}.json`,
+                params: this.paramsMapbox,
+            })
 
-        return []; // Retornar las ciudades coincidentes
+            const resp = await instance.get();
+            console.log(resp.data);
+
+            return []; // Retornar las ciudades coincidentes
+        } catch (error) {
+            
+        }
+
     } 
 }
 
