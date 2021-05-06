b129a83c27ebe965d17810273928b88bc6fb090a
Anderson <andemartinezdev@gmail.com>
2021-05-06 11:47:37 -0500

    07-06 -> Servir contenido estatico
    - Se retorna un html dede un endpoint.

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
+// Servir contenido estatico
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html');
+})
 
 app.listen(8080);
