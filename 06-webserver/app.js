05cd31e5a38d957c6003289f3f911e8e53e9f4ec
Anderson <andemartinezdev@gmail.com>
2021-05-06 11:22:29 -0500

    07-04 -> Req y Res
    - Se hizo una prueba de una request con su response.

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -5,7 +5,7 @@ const http = require('http');
 http.createServer((req, res) => {
 
     res.write('Hola mundo');
-
+    res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end();
 })
 .listen(8080);
