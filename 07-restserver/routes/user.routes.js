11f48320cb751db07aada8496e0a1733ec64e8c8
Anderson <andemartinezdev@gmail.com>
2021-05-08 15:47:39 -0500

    09-12 -> Validator DB
    - Se hace la validacion de roles existentes, con la base de datos.

--- 07-restserver/routes/user.routes.js
+++ 07-restserver/routes/user.routes.js
@@ -2,6 +2,7 @@ const {Router} = require('express');
 const {check} = require('express-validator');
 const { getUsuarios, putUsuarios, postUsuarios, deleteUsuarios } = require('../controllers/usuario.controller');
 const { validarCampos } = require('../middlewares/validar-campos.middlewares');
+const Role = require('../models/role.model');
 
 
 const router = Router();
@@ -14,7 +15,11 @@ router.post('/',[
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('password', 'El password debe tener mas de 6 caracteres').isLength({min: 6}),
     check('correo', 'El correo no es válido').isEmail(),
-    check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    // check('rol', 'No es un rol valido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('rol').custom(async(rol = "") => {
+        const existe = await Role.findOne({rol});
+        if(!existe) throw new Error(`El rol ${rol} no existe`);
+    }),
     validarCampos,
 ], postUsuarios);
 
