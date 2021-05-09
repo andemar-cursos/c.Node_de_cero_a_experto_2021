8cdb16e37e1daab8f3fad39c0aef729645e69a92
Anderson <andemartinezdev@gmail.com>
2021-05-09 18:24:52 -0500

    10-07 -> Validaciones login
    - Se hicieron algunas validaciones para verificar que el usuario exista/habilitado y que la contrasena sea valida.

--- 07-restserver/controllers/auth.controller.js
+++ 07-restserver/controllers/auth.controller.js
@@ -1,10 +1,36 @@
 const { response } = require('express');
+const bcryptjs = require("bcryptjs");
+const Usuario = require('../models/usuario.model');
 
-const login = (req, res = response) => {
 
-    res.json({
-        msg: 'ok',
-    })
+const login = async(req, res = response) => {
+
+    const {correo, password} = req.body;
+
+    try {
+        // Verificar email existe
+        const usuario = await Usuario.findOne({correo});
+        if(!usuario) return res.status(400).json({msg:'Usuario/password no validos'});
+        
+        // Usuario activo
+        if(!usuario.estado) return res.status(400).json({msg:'Usuario no habilitado'})
+        
+        // verify password
+        const validPassword = bcryptjs.compareSync(password, usuario.password);
+        if(!validPassword) return res.status(400).json({msg:'Usuario/password no validos'});
+        
+        // Generar JWT
+
+        res.json({
+            msg: 'ok',
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            msg: 'Hable con el administrador'
+        })
+    }
+
 };
 
 
