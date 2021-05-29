e3dac729d51ff25f4843e328632ef9677765dba8
Anderson <andemartinezdev@gmail.com>
2021-05-29 16:26:37 -0500

    11-08 -> Create User Google
    - Se creo el usuario, cuando hace login y el usuario no existe. Si existe retornar el JWT.

--- 07-restserver/controllers/auth.controller.js
+++ 07-restserver/controllers/auth.controller.js
@@ -41,18 +41,46 @@ const login = async(req, res = response) => {
 const googleSignIn = async(req, res=response) => {
 
     const { id_token } = req.body;
-
+    
     try {
         
-        const userg = await googleVerify(id_token);
+        const {correo, nombre, img} = await googleVerify(id_token);
+        let usuario = await Usuario.findOne({correo});
+        
+        
+        if(!usuario) {
+
+            const data = {
+                nombre,
+                correo,
+                password: ':]',
+                img,
+                google: true,
+            };
+
+            usuario = new Usuario(data);
+
+            await usuario.save();
+        }
+        
+        // Usuario DB estado
+        if(!usuario.estado) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado.'
+            });
+        }
+
+        // Generar JWT
+        const token = await generateJWT(usuario.id);
 
         res.json({
-            msg: userg
+            usuario,
+            token,
         });
         
     } catch (error) {
         res.status(400).json({
-            msg: 'Token no valido'
+            msg: error
         })
     }
 
