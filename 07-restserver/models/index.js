33cdc351fc9b05437586c480163593a53cdfb45b
Anderson <andemartinezdev@gmail.com>
2021-05-30 16:53:26 -0500

    12-05 -> Categoria Model
    - Se creo el modelo de la categoria.
    - Se centralizo todos los modelos en el index.

---
+++ 07-restserver/models/index.js
@@ -0,0 +1,12 @@
+const Categoria = require('./categora.model');
+const Role = require('./role.model');
+const Server = require('./server.model');
+const Usuario = require('./usuario.model')
+
+
+model.exports = {
+    Categoria,
+    Role,
+    Server,
+    Usuario
+};
